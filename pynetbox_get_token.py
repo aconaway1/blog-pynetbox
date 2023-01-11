12f8d8f97f6fe08678b0d02c678d4b25389bff07
Aaron Conaway <40375373+aconaway1@users.noreply.github.com>
2023-01-11 10:34:30 -0500

    Initial commit

---
+++ pynetbox_get_token.py
@@ -0,0 +1,17 @@
+import pynetbox
+import pprint
+import yaml
+
+ENV_FILE = "env.yml"
+
+with open(ENV_FILE) as file:
+    env_vars = yaml.safe_load(file)
+
+nb_conn = pynetbox.api(url=env_vars['netbox_url'])
+
+token = nb_conn.create_token(env_vars['username'], env_vars['password'])
+
+
+pprint.pprint(dict(token))
+
+token.delete()
---
+++ pynetbox_query_filter_1.py
@@ -0,0 +1,29 @@
+"""
+Show all devices in Netbox grouped by site
+"""
+import pynetbox
+import yaml
+
+ENV_FILE = "env.yml"
+
+with open(ENV_FILE) as file:
+    env_vars = yaml.safe_load(file)
+
+nb_conn = pynetbox.api(url=env_vars['netbox_url'])
+
+token = nb_conn.create_token(env_vars['username'], env_vars['password'])
+
+sites = nb_conn.dcim.sites.all()
+
+for site in sites:
+    site_header = f"Devices at site {site.name}"
+    print(site_header)
+    print("-" * len(site_header))
+    devices = nb_conn.dcim.devices.filter(site_id=site.id)
+    if len(devices) < 1:
+        print("No devices.")
+        continue
+    for device in devices:
+        print(f"{device.name:^20} {device.device_role.name:^20}")
+        
+token.delete()
